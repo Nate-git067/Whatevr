3aa763fa8e6afacd8fb0239f2c9ff23a13a341d5
Nate <natematora67@gmail.com>
2026-08-28 21:36:31 +0000

    Updated commit.

--- server/config/config.py
+++ server/config/config.py
@@ -1,4 +1,5 @@
 #Configuration file for the server -> APIs, databases URIs ...
+from sqlalchemy.orm import DeclarativeBase
 from dotenv import load_dotenv
 import os
 
@@ -7,6 +8,9 @@ load_dotenv() #loads the vairbales in .env into memory
 
 """Configuring the server's databases"""
 #relational -> Postgres 
+class Base(DeclarativeBase):
+    pass
+
 POSTGRES_URI = os.getenv('POSTGRES_URI')
 if not POSTGRES_URI:
     raise ValueError('Unable to retrieve the Postgres URI from the .env file.')
