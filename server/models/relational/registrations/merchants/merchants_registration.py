6d43c7d25b60093c9dec416b2e9d6d4ee00bc4e3
Nate <natematora67@gmail.com>
2026-08-28 22:43:18 +0000

    Finshed the buisness registration model. Starting the landing page register routes.

--- server/models/relational/registrations/merchants/merchants_registration.py
+++ server/models/relational/registrations/merchants/merchants_registration.py
@@ -4,10 +4,7 @@ from sqlalchemy import (
     String,
     DateTime
 )
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column
-)
+from sqlalchemy.orm import mapped_column
 from pydantic import EmailStr
 from pydantic_extra_types.phone_numbers import PhoneNumber
 from datetime import datetime
@@ -18,13 +15,13 @@ class MerchantRegistration(Base):
     __tablename__ = 'merchant_registration'
 
     #personal info of the table 
-    first_name:Mapped[str] = mapped_column(String(length=50), nullable=False)
-    last_name:Mapped[str] = mapped_column(String(length=50), nullable=False)
-    store_name:Mapped[str] = mapped_column(String(length=100), nullable=False)
+    first_name:str = mapped_column(String(length=50), nullable=False)
+    last_name:str = mapped_column(String(length=50), nullable=False)
+    store_name:str = mapped_column(String(length=100), nullable=False)
 
     #contact info in the table 
-    email:Mapped[EmailStr] = mapped_column(String(length=50), nullable=False, index=True)
-    phonenumber:Mapped[PhoneNumber] = mapped_column(String(length=15), nullable=False, index=True)
+    email:str = mapped_column(String(length=50), nullable=False, index=True)
+    phonenumber:str = mapped_column(String(length=15), nullable=False, index=True)
 
     #audit info 
-    created_at:Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    created_at:datetime = mapped_column(DateTime(timezone=True))
